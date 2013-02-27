f4284e433148715b7053effa462c353087d55bf6
mschneider <markus.schneider73@gmail.com>
2013-02-27 15:29:37 +0100

    setenv.sh was improved.

--- groovy/setenv.sh
+++ groovy/setenv.sh
@@ -1,4 +1,7 @@
-GROOVY_HOME=""
-PATH=$PATH:$GROOVY_HOME/bin
-CLASSPATH=$CLASSPATH:$PWD/change-rules/src
+GROOVY_HOME="/opt/local/bin/groovy/groovy-2.0.6"
+GRADLE_HOME="/opt/local/bin/gradle/gradle-1.3"
+PATH=$PATH:$GROOVY_HOME/bin:$GRADLE_HOME/bin
+CLASSPATH=$CLASSPATH:$BASENAME/src
 export CLASSPATH
+export GROOVY_HOME
+export GRADLE_HOME
